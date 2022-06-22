b8a31b09a80b93014e9da5e9753eacd7ee292b7b
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2022-06-22 18:14:16 +0300

    File set

---
+++ weatherApp/Model/Weather.swift
@@ -0,0 +1,8 @@
+//
+//  Weather.swift
+//  weatherApp
+//
+//  Created by Furkan Erzurumlu on 22.06.2022.
+//
+
+import Foundation
---
+++ weatherApp/Router/Router.swift
@@ -0,0 +1,13 @@
+//
+//  Router.swift
+//  weatherApp
+//
+//  Created by Furkan Erzurumlu on 22.06.2022.
+//
+
+import Foundation
+import UIKit
+
+final class Router {
+    static var shared: Router = Router()
+}
